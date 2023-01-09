b41509dd055a978b0c96712d5c28d0769be2caaa
Alex Helber <helber-alx@mail.ru>
2023-01-09 23:41:04 +0300

    btn fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,15 +5,15 @@
                 <a class="btn btn-ghost normal-case text-xl">IT-аутсорсинг</a>
             </div>
             <div class="flex-none">
-                <button class="btn btn-square btn-ghost">
+                <!-- <button class="btn btn-square btn-ghost">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         class="inline-block w-5 h-5 stroke-current">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z">
                         </path>
                     </svg>
-                </button>
-                <label for="my-modal-3" class="btn">open modal</label>
+                </button> -->
+                <label for="my-modal-3" class="btn btn-ghost">✉️</label>
 
             </div>
         </div>
