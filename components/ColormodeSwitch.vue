384c0e72924c0dd9e030bc0067d6610f772daa4d
Alex Helber <helber-alx@mail.ru>
2023-01-11 13:48:27 +0300

    a

--- components/ColormodeSwitch.vue
+++ components/ColormodeSwitch.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <!-- <h1>Color mode: {{ $colorMode.value }}</h1> -->
     <button aria-label="Color Mode" class="inline-block" @click="onClick">
-  <ColorScheme placeholder="...">
-    <Icon v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" size="32"/>
-    <Icon v-else name="heroicons-outline:sun" size="32"/>
-  </ColorScheme>
-</button>
+      <ColorScheme placeholder="...">
+        <Icon v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" size="32" />
+        <Icon v-else name="heroicons-outline:sun" size="32" />
+      </ColorScheme>
+    </button>
   </div>
 </template>
 <script setup>
-  const colorMode = useColorMode()
-  const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
+const colorMode = useColorMode()
+const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
 </script>
