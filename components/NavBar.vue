eb6ec29fc83f83ef1237267c5fdb1b4895f43ebf
Alex Helber <helber-alx@mail.ru>
2023-01-11 12:09:01 +0300

    title fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="navbar bg-base-100">
             <div class="flex-1">
-                <a class="btn btn-ghost normal-case text-xl">IT-аутсорсинг</a>
+                <a class="btn btn-ghost normal-case text-xl">IT-аутсорсинг в Сочи</a>
             </div>
             <div class="flex-none space-x-2">
                 <ContactsDropdown />
